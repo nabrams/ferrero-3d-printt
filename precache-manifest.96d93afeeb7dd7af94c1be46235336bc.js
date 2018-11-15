78a3e1ba1a4212b2a7273b47135ba2caa2643ed6
nabrams <nabrams94@gmail.com>
2018-11-15 18:22:52 -0500

    Updates

---
+++ precache-manifest.96d93afeeb7dd7af94c1be46235336bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a62d14e644b07bd3fe38f1a1a938ff4",
+    "url": "/kinder-exp/static/media/logo.6a62d14e.jpeg"
+  },
+  {
+    "revision": "2959f45db834f81a8e3f9d02fb3b2880",
+    "url": "/kinder-exp/static/media/3d.2959f45d.jpeg"
+  },
+  {
+    "revision": "3199ac6457b009a9bd8e",
+    "url": "/kinder-exp/static/js/runtime~main.3199ac64.js"
+  },
+  {
+    "revision": "682c98a96881c0b39c9f",
+    "url": "/kinder-exp/static/js/main.682c98a9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/kinder-exp/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "682c98a96881c0b39c9f",
+    "url": "/kinder-exp/static/css/main.8d96f1cf.chunk.css"
+  },
+  {
+    "revision": "6c45c8bc48f3a85c79e99e22b9fff8bb",
+    "url": "/kinder-exp/index.html"
+  }
+];
