bfd787f84ad001dca63a6fd14e15b8d18043de4a
nabrams <nabrams94@gmail.com>
2018-11-15 16:55:02 -0500

    update page

--- kinder-exp/src/App.js
+++ kinder-exp/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import kinder from './images/3d.jpeg';
+import logo from './images/logo.jpeg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={kinder} width="100%" /> */}
-          <p>
-            A whole new world, the Kinder Expierence
-          </p>
-        
-        </header>
+      
+      <div className="App" >
+      <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"></link>
+      <div className="App-background">
+      </div>
+    <div className="App-header">
+    <img className="App-logo" src={logo} width="30%" height="40%" />
+   <div className="App-container">
+    <b className="App-text" >We are kinder. A new and exciting expierence openening soon.
+    Enter your email to be the first to find out the details! </b>
+    
+    <form className="App-input" >
+      Email: <input type="text" name="email"/><br/>
+      <input className="App-input" type="submit" value="Submit" onClick={myFunction}/>
+     </form>
+    </div>
+    </div>
       </div>
     );
   }
 }
+
+function myFunction() {
+  console.log("HI");
+  }
 
 export default App;
